fbb414b04ca3eaf6f72d10235b39d49a3df36bcf
alejandro <54691090+Ale-cm@users.noreply.github.com>
2021-10-01 01:04:35 -0300

    OK: crear base de datos,tabla segun producto, Guardar en base de datos mysql

--- src/test/java/mercadoLibre/Home/TestPage.java
+++ src/test/java/mercadoLibre/Home/TestPage.java
@@ -7,33 +7,43 @@ import com.org.PruebaWebScrapp.mercadoLibre.HomeMeli;
 import com.org.PruebaWebScrapp.mercadoLibre.PageBusqueda;
 import com.org.PruebaWebScrapp.mercadoLibre.PageProduct;
 
+import bdmysqljava.BaseDatosCUp;
+import bdmysqljava.Conexion;
 import helpers.Helpers;
 /**
- * TestPage Clase principal
- * @author Alejandro
+ * TestPage Clase principal 
+ * @author Miguel A. Cabrera
  *
  */
 public class TestPage {
 
 	public static void main(String[] args) throws AWTException, InterruptedException, MalformedURLException {
-	WebDriver driver;
+		String baseDatos="mercadoli";
+		String producto="zapatillas";
+		WebDriver driver;
+		BaseDatosCUp bdup= new BaseDatosCUp();
+		Conexion c=new Conexion();
 		HomeMeli  MLhome =new HomeMeli(); 
 		Helpers help= new Helpers();
 		System.setProperty("webdriver.chrome.driver", help.detectorOS());
 		driver = new ChromeDriver();
+		bdup.crearBD(baseDatos, c);
+		bdup.crearTabla(baseDatos, producto, c);
 		PageBusqueda  MLpageBusq= new PageBusqueda(driver);
-		PageProduct MLpageProd=new PageProduct(driver);
+		PageProduct MLpageProd=new PageProduct(driver,baseDatos,producto);
 		MLhome.iniciarDriver(driver);
 		help.MaxPantalla(driver);
 		MLhome.buscar("https://www.mercadolibre.com.ar/");	//TODO	encasular en ap ml
-		MLhome.EscribeBusqueda("zapatillas");
+		MLhome.EscribeBusqueda(producto);
+		
+		
 		//**DBG: System.out.println(MLpbusq.obtenerUrls());
 		MLpageProd.guardarDatos(MLpageBusq.obtenerUrls());
+		
 		MLpageProd.mostrar();
 		help.espera_S(3);	
 		driver.close();	
 	}
-	
 }
 
  
